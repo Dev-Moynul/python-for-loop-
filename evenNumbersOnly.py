8a93b6a658882afde6e81629f4ea32abfc6be7e7
Dev-Moynul <stu.moynulislam@gmail.com>
2026-04-14 11:46:51 +0600

    even numbers only

---
+++ evenNumbersOnly.py
@@ -0,0 +1,10 @@
+'''num=int(input())
+for x in range(1,num+1):
+    if x%2==0:
+            print(x,end=" ")'''
+
+
+# better solution
+n=int(input())
+for x in range(2,n+1,2):
+      print (x,end=" ")
